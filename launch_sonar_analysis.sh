f93187a87d9ed49469d2282f211cff2d83db745e
Mathieu SOUM <mathieu.soum@gmail.com>
2014-10-16 22:32:04 +0200

    desabling host fp verification in ssh config

--- launch_sonar_analysis.sh
+++ launch_sonar_analysis.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-ssh -o StrictHostKeyChecking=no admin@vannier.pro -p 2276 /var/sonar_sources/Bar-O-Maitre/task.sh
+ssh admin@vannier.pro -p 2276 /var/sonar_sources/Bar-O-Maitre/task.sh
