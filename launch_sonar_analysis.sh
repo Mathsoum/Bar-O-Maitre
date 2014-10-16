dfe272c3922ee7ca2aaaa53c4db10fb83aa55076
Mathieu SOUM <mathieu.soum@gmail.com>
2014-10-16 22:56:26 +0200

    adding \n

--- launch_sonar_analysis.sh
+++ launch_sonar_analysis.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo "yes" | ssh admin@vannier.pro -p 2276 /var/sonar_sources/Bar-O-Maitre/task.sh
+echo "yes\n" | ssh admin@vannier.pro -p 2276 /var/sonar_sources/Bar-O-Maitre/task.sh
