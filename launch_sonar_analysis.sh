2eaddf934862506892efd776e1fcca72c4429c67
Mathieu SOUM <mathieu.soum@gmail.com>
2014-10-17 09:06:25 +0200

    fix .travis.yml. trying ssh auto-auth.

--- launch_sonar_analysis.sh
+++ launch_sonar_analysis.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo "yes\n" | ssh admin@vannier.pro -p 2276 /var/sonar_sources/Bar-O-Maitre/task.sh
+ssh -p'2276' -l'admin' -i'~/.ssh/id_rsa' vannier.pro /var/sonar_sources/Bar-O-Maitre/task.sh
