3de8822740959c6133e20b2e94ad117e24972bbc
yluom <leo.mouly@gmail.com>
2014-10-17 09:10:54 +0200

    User domain class initiated

--- grails-app/domain/bar/o/maitre/User.groovy
+++ grails-app/domain/bar/o/maitre/User.groovy
@@ -1,10 +1,22 @@
 package bar.o.maitre
+import groovy.time.*
+
 
 class User {
+    String nickname
     String firstName
     String lastName
+    String mail
     Date birthDate
 
     static constraints = {
+        nickname blank: false, nullable: false
+        firstName blank: false, nullable: false
+        lastName blank: false, nullable: false
+        mail email:true
+
+        use(TimeCategory) {
+            birthDate max: (new Date() - 18.years)
+        }
     }
 }
