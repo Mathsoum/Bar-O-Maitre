a9ba4f7f98ea6fe7290ab468cd52eca529634160
yluom <leo.mouly@gmail.com>
2014-10-17 11:22:21 +0200

    Added new User test on mail unique constraint

--- test/unit/bar/o/maitre/UserSpec.groovy
+++ test/unit/bar/o/maitre/UserSpec.groovy
@@ -8,11 +8,12 @@ import spock.lang.Specification
  */
 @TestFor(User)
 class UserSpec extends Specification {
-    User user1, user2
+    User user1, userWithSameNick, userWithSameMail
 
     def setup() {
-        user1 = new User(nickname:"mynick", firstName:"myfirstname", lastName:"mylastname", mail:"user1@mail.mail", birthDate:new Date("1/1/1980"))
-        user2 = new User(nickname:"mynick", firstName:"myfirstname", lastName:"mylastname", mail:"user2@mail.mail", birthDate:new Date("2/1/1980"))
+        user1 = new User(nickname:"mynick", firstName:"user1", lastName:"mylastname", mail:"user1@mail.mail", birthDate:new Date("1/1/1980"))
+        userWithSameNick = new User(nickname:"mynick", firstName:"myfirstname", lastName:"mylastname", mail:"user2@mail.mail", birthDate:new Date("2/1/1980"))
+        userWithSameMail = new User(nickname:"userWithSameMailThanUser1", firstName:"myfirstname", lastName:"mylastname", mail:"user1@mail.mail", birthDate:new Date("2/1/1980"))
     }
 
     def cleanup() {
@@ -21,15 +22,30 @@ class UserSpec extends Specification {
     def "test add two users with the same nick"() {
         given: "two users with the same nick"
         user1
-        user2
+        userWithSameNick
 
         when: "we add them to the database"
-        user1.save(failOnError: true)
-        user2.save()
+        user1.save(failOnError: true,flush: true)
+        userWithSameNick.save(flush: true)
+
+        then: "the first user was saved but the second user was not saved"
+        User.findByFirstName("user1") != null
+        User.findById(2) == null
+
+    }
+
+    def "test add two users with the same mail"() {
+        given: "two users with the same mail"
+        user1
+        userWithSameMail
+
+        when: "we add them to the database"
+        user1.save(failOnError: true, flush: true)
+        userWithSameMail.save(flush: true)
 
         then: "the first user was saved but the second user was not saved"
         User.findByNickname("mynick") != null
-        User.findById(2) == null
+        User.findByNickname("userWithSameMailThanUser1") == null
 
     }
 }
