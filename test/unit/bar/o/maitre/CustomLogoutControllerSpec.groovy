0b9ae671fabacc9afb1b113ae6632b61a3e10ac2
Mathieu SOUM <mathieu.soum@gmail.com>
2014-11-13 09:39:49 +0100

    #22 custom logout controller. logout possible here /Bar-O-Maitre/customLogout o/

---
+++ test/unit/bar/o/maitre/CustomLogoutControllerSpec.groovy
@@ -0,0 +1,20 @@
+package bar.o.maitre
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(CustomLogoutController)
+class CustomLogoutControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
